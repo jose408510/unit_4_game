6e3ca29dcb01c458105ad2961f1ebda23f1441ac
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-04-30 17:19:50 -0700

    more stuff added

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var winner = 0;
+var losser = 0;
+var greenBox = 0;   
+
+
+for (var i = 0; i < greenBox.length; i++);
+document.onkeyup = function(event){
+    var userScore = event.key.toLowerCase();
+}
+
+$('#winner').html(0);
+$('#losser').html(0);
+$('#greenBox').html(0);
+$('#oneMore').html(0);   
+
